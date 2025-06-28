1570334a69e8cabbd3a8d463fdc7832937fb5668
Alina <alinakotova102@gmail.com>
2025-06-28 15:00:17 +0200

    modified start point

--- src/task1_collections_framework/Runner.java
+++ src/task1_collections_framework/Runner.java
@@ -10,8 +10,18 @@ public class Runner {
         String textByFile = text.readFile("src/task1_collections_framework/file/text.txt");
         System.out.println(textByFile);
 
+        System.out.println("\nЧтение файла и вывод список слов: ");
         List<String> wordsByText = text.listWords(textByFile);
         System.out.println(wordsByText);
-        System.out.println(text.sortWordAsc(wordsByText));
+
+        System.out.println("\nСортировка слов игнорируя регистр: ");
+        System.out.println("Sort ASC: " + text.sortWordAsc(wordsByText));
+        System.out.println("Sort DESC: " + text.sortWordDesc(wordsByText));
+
+        System.out.println("\nСписок уникальных слов: ");
+        System.out.println(text.listUniqueWord(wordsByText));
+
+        System.out.println("\nКол-во дубликатов\n[слово - кол-во в тексте(без учета регистра)]");
+        System.out.println(text.infoDuplication(wordsByText));
     }
 }
